17289397bcc2a1b680880fde1e8bcb18c0d5de97
Myrzaim <mikosh9767@gmail.com>
2023-11-24 23:43:13 +0600

    Home page start

--- src/Components/HomePage/Homepage.jsx
+++ src/Components/HomePage/Homepage.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import Header from '../Header/Header';
 import './Homepage.css';
+import homelogo from '../Image/homelogo.png';
 
 const homepage = () => {
-    return (
-        <div>
-            
+    return (<>
+        <Header/>
+        <div className='first-div'>
+            <div className='d-flex div-second'>
+                <img src={homelogo} alt="homeLogo" className="homeLogo" />
+                <p className='first-txt'><span>Unlock</span> Your Creative Potential</p>
+          </div>
         </div>
-    );
+        </> );
 };
 
 export default homepage;
