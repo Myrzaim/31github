228b8549610de893eba05a78668c8513e131245b
Myrzaim <mikosh9767@gmail.com>
2023-11-25 17:54:12 +0600

    home page continue

--- src/Components/HomePage/Homepage.jsx
+++ src/Components/HomePage/Homepage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../Header/Header';
 import './Homepage.css';
 import homelogo from '../Image/homelogo.png';
+import { Button } from 'reactstrap';
 
 const homepage = () => {
     return (<>
@@ -10,7 +11,18 @@ const homepage = () => {
             <div className='d-flex div-second'>
                 <img src={homelogo} alt="homeLogo" className="homeLogo" />
                 <p className='first-txt'><span>Unlock</span> Your Creative Potential</p>
-          </div>
+            </div>
+           <div className='third-div'>
+                <p>with Online Design and Development</p>
+                <p> Courses.</p>
+                <h6>Learn from Industry Experts and Enhance Your Skills.</h6>
+                <Button color="warning">
+                Explore Courses
+  </Button>  <Button color="white">
+  View Pricing
+  </Button>
+                
+            </div>
         </div>
         </> );
 };
