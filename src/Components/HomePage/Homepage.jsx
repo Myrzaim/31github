b8db39bf1df20871fea06ff6bb2643a8f1560815
Myrzaim <mikosh9767@gmail.com>
2023-11-26 23:18:14 +0600

    home page continue

--- src/Components/HomePage/Homepage.jsx
+++ src/Components/HomePage/Homepage.jsx
@@ -3,6 +3,8 @@ import Header from '../Header/Header';
 import './Homepage.css';
 import homelogo from '../Image/homelogo.png';
 import { Button } from 'reactstrap';
+import { ListGroup, ListGroupItem } from 'reactstrap';
+import Logo from '../Image/Logo.png';
 
 const homepage = () => {
     return (<>
@@ -16,12 +18,45 @@ const homepage = () => {
                 <p>with Online Design and Development</p>
                 <p> Courses.</p>
                 <h6>Learn from Industry Experts and Enhance Your Skills.</h6>
-                <Button color="warning">
+                <Button>
                 Explore Courses
-  </Button>  <Button color="white">
+  </Button>  <Button className='button-white'>
   View Pricing
-  </Button>
-                
+  </Button>   
+            </div>
+            <div className='div-list'>
+            <ListGroup horizontal>
+    <ListGroupItem
+      href="#"
+      tag="a"
+    >
+      <img src = {Logo}></img>
+    </ListGroupItem>
+    <ListGroupItem
+      href="#"
+      tag="a"
+    >
+      Dapibus ac facilisis in
+    </ListGroupItem>
+    <ListGroupItem
+      href="#"
+      tag="a"
+    >
+      Morbi leo risus
+    </ListGroupItem>
+    <ListGroupItem
+      href="#"
+      tag="a"
+    >
+      Porta ac consectetur ac
+    </ListGroupItem>
+    <ListGroupItem
+      href="#"
+      tag="a"
+    >
+      Vestibulum at eros
+    </ListGroupItem>
+  </ListGroup> 
             </div>
         </div>
         </> );
