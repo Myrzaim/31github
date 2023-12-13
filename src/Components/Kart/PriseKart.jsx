8bba72243d79cec077f8dabd22ac25511a18302f
Myrzaim <mikosh9767@gmail.com>
2023-12-13 22:46:16 +0600

    prise card start

---
+++ src/Components/Kart/PriseKart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PriseKart.css';
+
+const PriseKart = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PriseKart;
