bcd2877f58a1f13cd19d537c95fcd3faefa8c285
Myrzaim <mikosh9767@gmail.com>
2023-12-17 22:04:20 +0600

    ask page start

--- src/Components/HomePage/Homepage.jsx
+++ src/Components/HomePage/Homepage.jsx
@@ -17,6 +17,7 @@ import { useContext, useEffect } from "react";
 import { courseContext } from "../../Context/CourseContextProvider";
 import KartCourses from "../Kart/KartCourses";
 import KartPost from "../Kart/KartPost";
+import AskKart from "../Kart/AskKart";
 
 const Homepage = () => {
   const {
@@ -260,45 +261,22 @@ const Homepage = () => {
             </p>
           </div>
           <div className="price_availability_item">
-            <svg
-              className="price_availability_item_svg"
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-            >
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M13.364 3.32297C13.6276 3.52379 13.6785 3.90027 13.4777 4.16385L7.07765 12.5639C6.97272 12.7016 6.81341 12.7873 6.64066 12.7989C6.46791 12.8105 6.29856 12.7469 6.17613 12.6245L2.57613 9.02449C2.34181 8.79018 2.34181 8.41028 2.57613 8.17596C2.81044 7.94165 3.19034 7.94165 3.42465 8.17596L6.5392 11.2905L12.5231 3.4366C12.724 3.17302 13.1004 3.12214 13.364 3.32297Z"
-                fill="#262626"
-              />
-            </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+<path d="M6.28033 5.21967C5.98744 4.92678 5.51256 4.92678 5.21967 5.21967C4.92678 5.51256 4.92678 5.98744 5.21967 6.28033L8.93934 10L5.21967 13.7197C4.92678 14.0126 4.92678 14.4874 5.21967 14.7803C5.51256 15.0732 5.98744 15.0732 6.28033 14.7803L10 11.0607L13.7197 14.7803C14.0126 15.0732 14.4874 15.0732 14.7803 14.7803C15.0732 14.4874 15.0732 14.0126 14.7803 13.7197L11.0607 10L14.7803 6.28033C15.0732 5.98744 15.0732 5.51256 14.7803 5.21967C14.4874 4.92678 14.0126 4.92678 13.7197 5.21967L10 8.93934L6.28033 5.21967Z" fill="#262626"/>
+</svg>
             <p className="price_availability_item_text">
               Access to exclusive Pro Plan community forums.
             </p>
           </div>
           <div className="price_availability_item">
-            <svg
-              className="price_availability_item_svg"
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-            >
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M13.364 3.32297C13.6276 3.52379 13.6785 3.90027 13.4777 4.16385L7.07765 12.5639C6.97272 12.7016 6.81341 12.7873 6.64066 12.7989C6.46791 12.8105 6.29856 12.7469 6.17613 12.6245L2.57613 9.02449C2.34181 8.79018 2.34181 8.41028 2.57613 8.17596C2.81044 7.94165 3.19034 7.94165 3.42465 8.17596L6.5392 11.2905L12.5231 3.4366C12.724 3.17302 13.1004 3.12214 13.364 3.32297Z"
-                fill="#262626"
-              />
-            </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+<path d="M6.28033 5.21967C5.98744 4.92678 5.51256 4.92678 5.21967 5.21967C4.92678 5.51256 4.92678 5.98744 5.21967 6.28033L8.93934 10L5.21967 13.7197C4.92678 14.0126 4.92678 14.4874 5.21967 14.7803C5.51256 15.0732 5.98744 15.0732 6.28033 14.7803L10 11.0607L13.7197 14.7803C14.0126 15.0732 14.4874 15.0732 14.7803 14.7803C15.0732 14.4874 15.0732 14.0126 14.7803 13.7197L11.0607 10L14.7803 6.28033C15.0732 5.98744 15.0732 5.51256 14.7803 5.21967C14.4874 4.92678 14.0126 4.92678 13.7197 5.21967L10 8.93934L6.28033 5.21967Z" fill="#262626"/>
+</svg>
             <p className="price_availability_item_text">
               Early access to new courses and updates.
             </p>
           </div>
+          <Button className="w-100">Get Started</Button>
         </div>
         <div className="pricing_div">
           <p className="price_title">Pro plan</p>
@@ -447,8 +425,11 @@ const Homepage = () => {
               Early access to new courses and updates.
             </p>
           </div>
+          <Button className="w-100">Get Started</Button>
         </div>
+     
       </div>
+      <AskKart/>
     </>
   );
 };
---
+++ src/Components/Kart/AskKart.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AskKart = () => {
+    return (<>
+        <div className='d-flex'>
+            <div className='w-30'>dfg</div>
+            <div>sdfg</div>
+            
+        </div>
+        </>
+    );
+};
+
+export default AskKart;
