d5b506f3c80b114f72c102c113f159ed42a1e46d
Myrzaim <mikosh9767@gmail.com>
2023-12-21 21:27:29 +0600

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import './Footer.css';
 import navLogo from '../Image/navLogo.png';
+import List from 'reactstrap'; 
 
 const Footer = () => {
     return (
         <div>
             <div>
+            <div className='navLogo'>
             <img src={navLogo} alt="navLogo" className="imgLogo" />
             </div> 
+            
+<List type="unstyled">
+  <li>
+    Lorem ipsum dolor sit amet
+  </li>
+  <li>
+    Consectetur adipiscing elit
+  </li>
+  <li>
+    Integer molestie lorem at massa
+                </li>
+                </List>
+                </div>
             <div></div> 
             <div></div> 
             <div></div> 
